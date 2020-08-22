e970691b214094ac698ba31229bbc9f698ecfd2b
Alex1114 <pokai.eed05@g2.nctu.edu.tw>
2020-08-22 16:56:26 +0800

    fix some bugs

--- HCC_competition_2020/environment.sh
+++ HCC_competition_2020/environment.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 source /opt/ros/melodic/setup.bash
-source ~/ROS-Projects/HCC_competition_2020/catkin_ws/devel/setup.bash
+source ~/HCC_competition_2020/catkin_ws/devel/setup.bash
 
 echo "HCC_competition_2020 environment"
